6eb98c2d9032d7ccb5725a1e383d27b29598de85
Decklin Foster <decklin@red-bean.com>
2010-01-03 11:37:44 -0500

    Bump version

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-const USER_AGENT = 'Explode/0.6';
+const USER_AGENT = 'Explode/0.7';
 const API_ROOT = 'http://api.longurl.org/v2/';
 const FETCH_DELAY = 800;
 const SERVICES_CACHE_TIME = 86400 * 1000;
